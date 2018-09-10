c2bea2953894a8b74eff1b403234bf1a7855f261
xsjado <marius.schenzle@gmx.de>
2018-09-10 21:35:49 +0200

    fixed header link to home-page

--- routes/web.php
+++ routes/web.php
@@ -62,5 +62,5 @@ Route::get('/facades/decrypt', function () {
 });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/generate/password', function() { return bcrypt('123456789'); });
